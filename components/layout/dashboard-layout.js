55a7cf9f09d6eeb48f32f3687bbdbe52efbc9ef5
AhmedKamran1 <ahmed.amigo3@gmail.com>
2024-01-07 01:00:18 +0500

    adjusted icons

--- components/layout/dashboard-layout.js
+++ components/layout/dashboard-layout.js
@@ -1,58 +1,58 @@
-import React, { useState } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 //Styles
-import { List, ListItem, ListItemIcon } from "@mui/material";
+import { List, ListItem, ListItemIcon } from '@mui/material';
 import {
   DrawerIcon,
   CustomDrawer,
   DrawerListText,
   DrawerListButton,
   DrawerListItem,
-} from "../UI";
+} from '../UI';
 
 //Icons
-import SummarizeIcon from "@mui/icons-material/Summarize";
-import EditNoteIcon from "@mui/icons-material/EditNote";
-import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
-import MenuBookIcon from "@mui/icons-material/MenuBook";
-import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import SummarizeIcon from '@mui/icons-material/Summarize';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 const DashboardLayout = ({ children }) => {
   const router = useRouter();
 
-  const value = router.asPath.split("/");
+  const value = router.asPath.split('/');
   const [selectedPage, setSelectedPage] = useState(value[value.length - 1]);
   const [open, setOpen] = useState(false);
 
   const dashboardLinks = [
     {
-      id: "overview",
-      text: "Overview",
+      id: 'overview',
+      text: 'Overview',
       icon: <SummarizeIcon />,
     },
     {
-      id: "restaurant-listing",
-      text: "Restaurant Listings",
+      id: 'restaurant-listing',
+      text: 'Restaurant Listings',
       icon: <EditNoteIcon />,
     },
     {
-      id: "modify-requests",
-      text: "Modify Requests",
-      icon: <MonetizationOnIcon />,
+      id: 'modify-requests',
+      text: 'Modify Requests',
+      icon: <MenuBookIcon />,
     },
     {
-      id: "logs",
-      text: "Logs",
+      id: 'logs',
+      text: 'Logs',
       icon: <WorkHistoryIcon />,
     },
     {
-      id: "payments",
-      text: "Payments",
-      icon: <MenuBookIcon />,
+      id: 'payments',
+      text: 'Payments',
+      icon: <MonetizationOnIcon />,
     },
   ];
 
@@ -85,14 +85,14 @@ const DashboardLayout = ({ children }) => {
             </Link>
           ))}
         </List>
-        <List sx={{ mt: "auto", mb: 5 }}>
+        <List sx={{ mt: 'auto', mb: 5 }}>
           <Link href={`/`}>
             <ListItem>
               <DrawerListButton>
                 <ListItemIcon>
                   <KeyboardArrowLeftIcon />
                 </ListItemIcon>
-                <DrawerListText primary={"Logout"} open={open} />
+                <DrawerListText primary={'Logout'} open={open} />
               </DrawerListButton>
             </ListItem>
           </Link>
