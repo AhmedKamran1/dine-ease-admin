fd6fd55f43389c2ff7b61aae7b4588e388e64216
AhmedKamran1 <ahmed.amigo3@gmail.com>
2024-01-07 01:00:52 +0500

    added restaurant service

---
+++ services/restaurant.js
@@ -0,0 +1,21 @@
+import api from './api';
+import { PORTS } from '@/utils/port';
+
+const service = 'restaurant';
+const port = PORTS[service];
+
+export const getApprovedRestaurants = () => {
+  return api.get(`http://localhost:${port}/api/${service}/approved`);
+};
+
+export const getAllRestaurants = () => {
+  return api.get(`http://localhost:${port}/api/${service}/all`);
+};
+
+export const getPendingRestaurants = () => {
+  return api.get(`http://localhost:${port}/api/${service}/pending`);
+};
+
+export const restaurantStatusUpdate = (id, payload) => {
+  return api.patch(`http://localhost:${port}/api/${service}/status/${id}`, payload);
+};
