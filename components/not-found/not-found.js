a6de47541432a76eb8b6e8de7491545abd91017a
Ahmed Kamran <ahmed.amigo3@gmail.com>
2024-04-05 15:00:57 +0500

    added not found page

---
+++ components/not-found/not-found.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+// Styles
+import { PrimaryButton, SecondaryContainer, Text } from '../UI';
+import { Box, Grid } from '@mui/material';
+
+// Icons
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import image from '@/public/assets/images/food.svg';
+
+const NotFound = () => {
+  const router = useRouter();
+
+  return (
+    <SecondaryContainer container sx={{ height: 'auto' }}>
+      <Grid item xs={12} sx={{ position: 'relative', height: '350px' }}>
+        <Image src={image} fill={true} sizes="100vw" alt="login-image" />
+      </Grid>
+      <Grid item xs={12} textAlign="center">
+        <Box mb={2}>
+          <Text variant="header" fontWeight={800}>
+            404
+          </Text>
+        </Box>
+        <Box mb={4}>
+          <Text variant="main">
+            Sorry, the page you are looking for could not be found
+          </Text>
+        </Box>
+        <PrimaryButton onClick={() => router.back()} startIcon={<ArrowBackIcon />}>
+          Go Back
+        </PrimaryButton>
+      </Grid>
+    </SecondaryContainer>
+  );
+};
+
+export default NotFound;
